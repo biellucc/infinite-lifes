bfeca8947ce4429edf9208439478fe95f73c2e18
Biellucc <gabriellucc02@gmail.com>
2024-10-22 15:20:15 -0300

    Adicionei um if no descriptar o cartão, caso seja um cliente, descripta todos os numero, caso n seja, apenas os 4 primeiros e últimos números.

--- app/Services/criptografiaService.php
+++ app/Services/criptografiaService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 
 class criptografiaService
@@ -16,9 +17,12 @@ class criptografiaService
         return $request;
     }
 
-    public function descriptografarCartao($request){
+    public function descriptografarCartao($request)
+    {
         $numero_descriptografado = Crypt::decryptString($request->numero);
-        $numero_descriptografado = substr_replace($numero_descriptografado, ' **** **** ', 4, -4);
+        if (!Auth::check() || !Auth::user()->cliente) {
+            $numero_descriptografado = substr_replace($numero_descriptografado, ' **** **** ', 4, -4);
+        }
         $validade_descriptografada = Crypt::decryptString($request->validade);
         $request->numero = $numero_descriptografado;
         $request->validade = $validade_descriptografada;
@@ -26,12 +30,14 @@ class criptografiaService
         return;
     }
 
-    public function criptografarCpf($cpf)  {
+    public function criptografarCpf($cpf)
+    {
         $cpf = Crypt::encryptString($cpf);
         return $cpf;
     }
 
-    public function descriptografarCpf($cpf)  {
+    public function descriptografarCpf($cpf)
+    {
         $cpf_descriptografado = Crypt::decryptString($cpf);
         $cpf_descriptografado = substr_replace($cpf_descriptografado, '***.*** ', 4, -3);
         $cpf = $cpf_descriptografado;
@@ -39,12 +45,14 @@ class criptografiaService
         return $cpf;
     }
 
-    public function criptografarCnpj($cnpj)  {
+    public function criptografarCnpj($cnpj)
+    {
         $cnpj = Crypt::encryptString($cnpj);
         return $cnpj;
     }
 
-    public function descriptografarCnpj($cnpj)  {
+    public function descriptografarCnpj($cnpj)
+    {
         $cnpj_descriptografado = Crypt::decryptString($cnpj);
         $cnpj_descriptografado = substr_replace($cnpj_descriptografado, '.***.*** ', 2, -8);
         $cnpj = $cnpj_descriptografado;
