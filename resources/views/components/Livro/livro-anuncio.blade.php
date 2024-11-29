7d35a5913e7170d60b349b323e273895d88c012f
Biellucc <gabriellucc02@gmail.com>
2024-11-29 15:30:02 -0300

    Arrumei o tamanho do card dos produtos (tela principal). Alterei um pouco o svg_images

--- resources/views/components/Livro/livro-anuncio.blade.php
+++ resources/views/components/Livro/livro-anuncio.blade.php
@@ -1,19 +1,28 @@
 @props(['id'])
-
 <div class="col mx-4 my-2">
-    <div class="card shadow-sm">
+    <div class="card shadow-sm h-100 d-flex flex-column" style="min-height: 400px; max-height: 400px">
         <x-livro.svg_imagem>
             <x-slot name="imagem">{{ $imagem }}</x-slot>
             <x-slot name="titulo">{{ $titulo }}</x-slot>
         </x-livro.svg_imagem>
-        <div class="card-body">
-            <x-h1 class="card-title">{{ $titulo }}</x-h1>
-            <p class="card-text">{{ $resumo }}</p>
-            <p class="card-text"><small class="text-body-secondary">
-                    {{ $tempo }}
-                </small>
+        <div class="card-body d-flex flex-column">
+            <!-- Título do Livro -->
+            <x-h1 class="card-title mb-2">
+                {{ $titulo }}
+            </x-h1>
+
+            <!-- Resumo com limite -->
+            <p class="card-text flex-grow-1">
+                {{ Str::limit($resumo, 40, '...') }}
             </p>
-            <div class="d-flex justify-content-between align-items-center">
+
+            <!-- Informações adicionais -->
+            <p class="card-text mt-auto"><small class="text-body-secondary">
+                {{ $tempo }}
+            </small></p>
+
+            <!-- Botão -->
+            <div class="d-flex justify-content-between align-items-center mt-3">
                 <div class="btn-group">
                     <form action="{{ route('site.livro', ['titulo' => $titulo ?? null, 'id' => $id]) }}" method="get">
                         <x-buttons.information_button />
--- resources/views/components/Livro/svg_imagem.blade.php
+++ resources/views/components/Livro/svg_imagem.blade.php
@@ -1,7 +1,7 @@
-@props(['x' => 1, 'y' => 1, 'width' => '100%', 'height' => '100%' ])
+@props(['x' => 1, 'y' => 1, 'width' => '100%', 'height' => '100%'])
 
-<svg class="bd-placeholder-img card-img-top" width="{{ $width }}" height="{{ $height }}" xmlns="http://www.w3.org/2000/svg" role="img"
+<svg class="bd-placeholder-img card-img-top p-3" width="{{ $width }}" height="{{ $height }}" xmlns="http://www.w3.org/2000/svg" role="img"
     aria-label="Espaço reservado: Miniatura" preserveAspectRatio="xMidYMid slice" focusable="false">
-    <image x="{{ $x }}" y="{{ $y }}" width="{{ $width }}" height="{{ $height }}" alt="{{ $titulo }}"
+    <image x="0" y="0" width="{{ $width }}" height="{{ $height }}" alt="{{ $titulo }}"
         href="/assets/livro/imagem/{{ $imagem }}" />
 </svg>
